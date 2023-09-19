73b5b679679abfc883b37fd78b4bb2cd3dd44cf1
Yosef Lachman <46474203+Persuantgecko80@users.noreply.github.com>
2023-09-19 12:34:34 +0300

    creating the grid function

---
+++ scripts.js
@@ -0,0 +1,16 @@
+const container = document.querySelector('.container');
+
+
+
+function createGrid (gridNumber) {
+    let gridArea = gridNumber * gridNumber;
+    for (let i = 1; i <= gridArea; i++) {
+        let gridItem = document.createElement('div');
+        container.style.gridTemplateColumns = `repeat(${gridNumber}, 1fr)`;
+        container.style.gridTemplateRows = `repeat(${gridNumber}, 1fr)`;
+        container.insertAdjacentElement('beforeend', gridItem);
+    }
+    let gridPixels = container.querySelectorAll('div');
+    gridPixels.forEach(gridPixels => gridPixels.addEventListener('mouseover', 'black'));
+}
+
