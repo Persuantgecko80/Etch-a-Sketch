55fd8630ea1f1413e55cdf54b15d17e8cc3006c0
Yosef Lachman <46474203+Persuantgecko80@users.noreply.github.com>
2023-09-20 11:15:29 +0300

    Create Board Function

--- script.js
+++ script.js
@@ -8,4 +8,12 @@ function createBoard(size){
 
     board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    let numDivs = size * size;
+
+    for(let i = 0; i < numDivs; i++){
+        let div = document.createElement("div");
+        div.style.backgroundColor = "yellow";
+        board.insertAdjacentElement("beforeend", div);
+    }
 }
